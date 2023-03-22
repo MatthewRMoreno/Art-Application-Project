5ae1d22296ab2b520d4e7518475d774b7938339b
Matthew Moreno <matthew.r.moreno21@gmail.com>
2023-03-22 03:28:03 -0500

    Rectangle is working and able to resize the shape

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const canvas = document.querySelector('canvas');
+toolBtns = document.querySelectorAll(".tool");
 ctx = canvas.getContext('2d');
 
-let isDrawing = false;
+// Global variables with default values
+let prevMouseX, prevMouseY, snapshot,
+isDrawing = false,
+selectedTool = "brush",
+brushWidth = 5;
 
 window.addEventListener("load", () => {
     // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
@@ -9,16 +14,41 @@ window.addEventListener("load", () => {
     canvas.height = canvas.offsetHeight;
 })
 
-const startDraw = () => {
+const drawRect = (e) => {
+    ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+}
+
+const startDraw = (e) => {
     isDrawing = true;
+    prevMouseX = e.offsetX; //  passing current mouseX position as prevMouseX value
+    prevMouseY = e.offsetY; // passing current mouseY position as prevMouseY value
     ctx.beginPath(); // creating new path for drawing
+    ctx.lineWidth = brushWidth; // passing brushSize as line width
+    // copying canvas data & passing as snapshot value.. this aboids dragging the image
+    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
 const drawing = (e) => {
     if(!isDrawing) return; // if isDrawing is false return from here
+    ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on to this canvas
+
+    if(selectedTool === "brush") {
     ctx.lineTo(e.offsetX, e.offsetY); // creating a line according to the mouse position
     ctx.stroke(); // drawing/filling the line with color
+    } else if(selectedTool === "rectangle") {
+        drawRect(e);
+    }
 }
+
+toolBtns.forEach(btn => {
+    btn.addEventListener("click", () => { // adding click event to all tool options
+        //removing active class from the previous option and adding on current clicked option
+        document.querySelector(".options .active").classList.remove("active");
+        btn.classList.add("active");
+        selectedTool = btn.id;
+        console.log(btn.id);
+    })
+})
 
 canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mousemove', drawing);
