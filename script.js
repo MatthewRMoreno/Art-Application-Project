e93d625d1b1f61667140ec6ddc9df703b880fcfc
Matthew Moreno <matthew.r.moreno21@gmail.com>
2023-03-23 16:20:00 -0500

    Added a function to be able to choose the color

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const canvas = document.querySelector('canvas');
 toolBtns = document.querySelectorAll(".tool");
 fillColor = document.querySelector("#fill-color");
-sizeSlider = document.querySelector("#size-slider")
+sizeSlider = document.querySelector("#size-slider");
+colorBtns = document.querySelectorAll(".colors .option");
+colorPicker = document.querySelector("#color-picker");
+clearCanvas = document.querySelector(".clear-canvas");
 ctx = canvas.getContext('2d');
 
 // Global variables with default values
@@ -9,6 +12,7 @@ let prevMouseX, prevMouseY, snapshot,
 isDrawing = false,
 selectedTool = "brush",
 brushWidth = 5;
+selectedColor = "#000";
 
 window.addEventListener("load", () => {
     // setting canvas width/height.. offsetwidth/height returns viewable width/height of an element
@@ -49,6 +53,8 @@ const startDraw = (e) => {
     prevMouseY = e.offsetY; // passing current mouseY position as prevMouseY value
     ctx.beginPath(); // creating new path for drawing
     ctx.lineWidth = brushWidth; // passing brushSize as line width
+    ctx.strokeStyle = selectedColor; // passing selectedColor as stroke style
+    ctx.fillStyle = selectedColor; // passing selectedColor as fill style
     // copying canvas data & passing as snapshot value.. this aboids dragging the image
     snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
@@ -57,7 +63,9 @@ const drawing = (e) => {
     if(!isDrawing) return; // if isDrawing is false return from here
     ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on to this canvas
 
-    if(selectedTool === "brush") {
+    if(selectedTool === "brush" || selectedTool === "eraser") {
+        // if selected tool is eraser then set strokeStyle to white to paint white color on the existing canvas conent else set tje stroke color to selected color
+        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
     ctx.lineTo(e.offsetX, e.offsetY); // creating a line according to the mouse position
     ctx.stroke(); // drawing/filling the line with color
     } else if(selectedTool === "rectangle") {
@@ -80,6 +88,26 @@ toolBtns.forEach(btn => {
 });
 
 sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value); // passing slider value as brushSize
+
+colorBtns.forEach(btn => {
+    btn.addEventListener("click", () => { // adding click event to all color options
+        //removing selected class from the previous option and adding on current clicked option
+        document.querySelector(".options .selected").classList.remove("selected");
+        btn.classList.add("selected");
+        // passing selected btn background as selectedColor value
+        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+    });
+});
+
+colorPicker.addEventListener("change", () => {
+    // passing picked color value from the color picker to last color btn background
+    colorPicker.parentElement.style.background = colorPicker.value;
+    colorPicker.parentElement.click();
+});
+
+clearCanvas.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clearing the whole canvas
+});
 
 canvas.addEventListener('mousedown', startDraw);
 canvas.addEventListener('mousemove', drawing);
